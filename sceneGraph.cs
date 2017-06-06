5d24d6ccf75944b49db2ee93f8d520e8ad9b6444
Brian <brianvanbeusekom@hotmail.com>
2017-06-06 11:55:47 +0200

    graph

---
+++ sceneGraph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Template_P3
+{
+    class sceneGraph
+    {
+        public List<Mesh> meshes = new List<Mesh>();
+
+        public sceneGraph()
+        {
+
+        }
+
+        public void loadMesh(string path)
+        {
+            meshes.Add(new Mesh(path));
+        }
+    }
+}
