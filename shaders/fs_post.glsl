c99a364436ecaf748481ec77adf4dfdeb790e1ea
Job van Zelm <jobvanzelm@gmail.com>
2017-06-23 14:48:13 +0200

    sdkfjao;sidjfo;as

--- shaders/fs_post.glsl
+++ shaders/fs_post.glsl
@@ -14,8 +14,9 @@ void main()
 	outputColor = texture( pixels, uv ).rgb;
 	// apply dummy postprocessing effect
 	float dx = P.x - 0.5, dy = P.y - 0.5;
-	float distance = -sqrt( dx * dx + dy * dy ) * 0.5f;
-	outputColor += distance;
+	float distance = -sqrt( dx * dx + dy * dy ) * 0.3f;
+	vec3 color = vec3(1.0f,5.0f,3.0f);
+	outputColor += distance * color;
 }
 
 // EOF
